86d702a59960fbafb7fc19e3aea4c03551496637
Sergey Nepomnyashiy <nepomnyashiy.s@gmail.com>
2025-11-14 23:39:35 +0300

    fix(ci): Remove unused import in alembic/env.py

--- backend/alembic/env.py
+++ backend/alembic/env.py
@@ -3,7 +3,7 @@ import sys
 from logging.config import fileConfig
 from pathlib import Path
 
-from sqlalchemy import engine_from_config, pool
+from sqlalchemy import pool
 
 from alembic import context
 
